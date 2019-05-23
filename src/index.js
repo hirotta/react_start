e70ec0d7b8175f043c70bb4ee427b3495ee3f024
nekopanda <kouhiro3782@yahoo.co.jp>
2019-05-24 02:49:03 +0900

    update index.js

--- src/index.js
+++ src/index.js
@@ -4,17 +4,12 @@ import './index.css';
 
 
 class Square extends React.Component {
-  constructor(props){
-    super(props);
-    this.state= { 
-      value: null,
-    };
-  }
   render() {
     return (
       <button
         className="square"
-        onClick={() => this.setState({value: 'X'})}>
+        onClick={() => this.props.onClick()}
+      >
         {this.state.value}
       </button>
     );
@@ -22,10 +17,21 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
-  renderSquare(i) {
-    return <Square value={i} />;
+  constructor(props){
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    }
   }
 
+  renderSquare(i){
+    return(
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
   render() {
     const status = 'Next player: X';
 
